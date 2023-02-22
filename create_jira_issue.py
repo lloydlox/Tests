f99c2eb5e0eb5a404e72e5e644847cf7dc7b1b17
lloydlox <lloydlox@yahoo.co.uk>
2023-02-22 14:30:33 +0000

    Testing webhook

--- create_jira_issue.py
+++ create_jira_issue.py
@@ -3,7 +3,7 @@ import json
 import os
 
 # Set the Jira URL and API endpoint
-url = 'https://afridao.atlassian.net/rest/api/2/issue/'
+jira_url = 'https://afridao.atlassian.net/rest/api/2/issue/'
 
 # Set your Jira API credentials
 user = 'lloydlox@yahoo.co.uk'
@@ -33,10 +33,37 @@ headers = {
 auth = (user, apikey)
 
 # Send a POST request to create the new issue
-response = requests.post(url, headers=headers, auth=auth, json=payload)
+response = requests.post(jira_url, headers=headers, auth=auth, json=payload)
 
 # Check the response status code
 if response.status_code == 201:
     print('Issue created successfully')
+    # Set up the API endpoint and headers
+    api_url = "https://your-jira-server.com/rest/webhooks/1.0/webhook"
+    headers = {"Content-Type": "application/json"}
+    # Set up the webhook payload
+    webhook_payload = {
+        "name": "My Webhook",
+        "url": "https://api.github.com/repos/zambezidao/urevise-frontend/dispatches",
+        "event_type": ["Jira Repository Dispatch Trigger"]
+    }
+    # Convert the payload to a JSON string
+    json_payload = json.dumps(webhook_payload)
+
+    # Send the API request to create the webhook
+    response = requests.post(api_url, data=json_payload, headers=headers, auth=("username", "password"))
+
+
+    # Check the response status code
+    if response.status_code == 200:
+        print("Webhook created successfully")
+    else:
+        print("Error creating webhook. Response:", response.text)
+    
 else:
     print('Failed to create issue: %s' % response.text)
+
+
+
+
+
